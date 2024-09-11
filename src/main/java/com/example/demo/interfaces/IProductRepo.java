eabc93b09558558efd7062bc9cd7528c1096dadb
Anurag Karan <anurag.karan.iert@gamil.com>
2024-09-11 23:43:02 +0530

    showBy ID fixed and custom query code added

--- src/main/java/com/example/demo/interfaces/IProductRepo.java
+++ src/main/java/com/example/demo/interfaces/IProductRepo.java
@@ -1,10 +1,15 @@
 package com.example.demo.interfaces;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.demo.model.Product;
 
 public interface IProductRepo extends MongoRepository<Product, Integer>{
 
+	Optional<Product> findByID(Integer ID);
+	Optional<Product> findByName(String Name);
+	Optional<Product> findByNameAndID(String name, Integer ID);
 	
 }
